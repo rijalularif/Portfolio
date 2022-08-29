a5124d7109d4769f08ab1a19bf21e74550ccc138
Rijalul Arif <74054304+rijalularif@users.noreply.github.com>
2022-08-29 22:11:19 +0700

    Add files via upload

---
+++ page/otomotif.php
@@ -0,0 +1,29 @@
+<div class="hal3">
+    <table align="center">
+		  <td align="center">
+        <img alt="file foto" src="img/contact.png" height="50px" />
+        <h4>
+          <font color="white">Contact me
+        </h4>
+	      <h4> Want to hear more about my services? Just send me a message with the form below, and I'll get back to you as soon as possible.
+          </font>
+        </h4>
+        <br>
+        <br>
+        <img alt="file foto" src="img/contt.png" height="400px" />
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <img alt="file foto" src="img/email.png" height="190px" />
+        <br>
+        <br>
+        <br>
+        <br>
+      </td>
+	</table>
+</div>
